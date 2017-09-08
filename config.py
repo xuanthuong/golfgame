e304fc57320a3ab21a8e28d700dc4eec4bb40bcc
ThuongTran <xuanthuongtran@gmail.com>
2017-09-08 11:04:52 +0700

    fix db_url

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ import os
 
 def get_DB_URL():
   # return os.environ['GOLF_GAME_DB_URL']
-  return "mysql://golf_user:dounets123!@localhost/golfgame"
+  return "mysql://golf_user:123789@gmf-db.dounets.com/gamification"
+  # return "mysql://golf_user:dounets123!@localhost/golfgame"
 
 def get_server_link():
   return "http://dounets.com:5003/api/socketApi"
